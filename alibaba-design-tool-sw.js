0e64ba767e8ada8e5fa0a6bdfcfb43ae565023c0
by5024011-jpg <by5024011@gmail.com>
2026-05-17 21:07:25 +0800

    Add files via upload

--- alibaba-design-tool-sw.js
+++ alibaba-design-tool-sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Alibaba Design Tool
 // Caches the main HTML + manifest for offline use
-const CACHE = 'alibaba-design-v3';
+const CACHE = 'alibaba-design-v4';
 const FILES = [
   './',
   './index.html',
